548f5ae625675e34a0ebe966a882f499643cb922
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2024-10-03 23:58:22 +0100

    update

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,18 +5,18 @@ def pascal_triangle(n):
         n (int): size of triangle
 
     Returns:
-        List: Triangle shapes 
+        List: Triangle shapes
     """
     if n <= 0:
         return []
 
-    pasTran = [[1]]  # Initialize with the first row [1]
+    triangle = [[1]]
 
     for i in range(1, n):
-        prev_row = pasTran[-1]  # Get the previous row
-        # Construct the new row by adding pairs of adjacent values from the previous row
-        new_row = [1] + [prev_row[j] + prev_row[j + 1] for j in range(len(prev_row) - 1)] + [1]
-        pasTran.append(new_row)
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
 
-    return pasTran
-
+    return triangle
